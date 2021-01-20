25b1d1071bebbd3dbefcd6a4f51640062836f82c
amirnaur <amir.naur@gmail.com>
2021-01-20 10:45:06 +0600

    rest_framework and Profile model

--- cal/models.py
+++ cal/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    TEMPLATES = [
+        ("table.css", "Таблица"),
+        ("modern.css", "Современный"),
+    ]
+    template = models.CharField(max_length=30, choices=TEMPLATES, default="modern.css", verbose_name="Шаблон")
+    show_year = models.BooleanField(default=True, verbose_name="Показывать год")
+    cross = models.BooleanField(default=True, verbose_name="Зачеркивание")
+
+    def __str__(self):
+        return self.user.username
+
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
