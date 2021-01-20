2883ab42feece6ca6bbc1ee8cd3cb0ac780157e1
amirnaur <amir.naur@gmail.com>
2021-01-20 11:35:31 +0600

    Profile model, rest_framework and migrations

--- cal/models.py
+++ cal/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class Profile(models.Model):
@@ -27,4 +28,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    try:
+        instance.profile.save()
+    except ObjectDoesNotExist:
+        Profile.objects.create(user=instance)
---
+++ cal/urls.py
@@ -0,0 +1,10 @@
+# from django.conf.urls import url
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# from . import views
+#
+# app_name = 'cal'
+#
+# urlpatterns = [
+#     url(r'^profile/', views.profile, name='profile_edit'),
+# ]
